403c30aba246d9cc41e1eda5eeb749b929076033
ingelieur <hacktiv8@hacktiv8s-Mac-mini.local>
2017-06-19 17:33:45 +0700

    save is successfull

--- source/bug-tracker/bug-tracker/src/App.js
+++ source/bug-tracker/bug-tracker/src/App.js
@@ -9,9 +9,9 @@ class App extends Component {
       <div>
         <div className="container">
           <h1 className="title is-1">Bug Tracker<small>by HACKTIV8</small></h1>
+          <BugsForm />
+          <ListOfBugs />
         </div>
-        <BugsForm />
-        <ListOfBugs />
         <Footer />
       </div>
     )
--- source/bug-tracker/bug-tracker/src/components/ListOfBugs.js
+++ source/bug-tracker/bug-tracker/src/components/ListOfBugs.js
@@ -1,10 +1,51 @@
 import React from 'react'
 
 class ListOfBugs extends React.Component {
+  fetchBugs () {
+    console.log('aaaaaaaa')
+    let bugs = JSON.parse(localStorage.getItem('bugs')) || []
+    let listBugsElement = document.getElementById('listBugs')
+
+    console.log(bugs)
+
+    listBugsElement.innerHTML = ''
+
+    for(let i = 0; i < bugs.length; i++) {
+      let id = bugs[i].id
+      let desc = bugs[i].description
+      let severity = bugs[i].severity
+      let assignedTo = bugs[i].assignedTo
+      let status = bugs[i].status
+
+      listBugsElement.innerHTML += `<div class="card">
+        <header class="card-header">
+          <p class="card-header-title">
+          BugId: ${id}
+          </p>
+        </header>
+        <div class="card-content">
+          <div class="content">
+            ${desc}
+            <span class="tag is-info">${severity}</span>
+            <p>Assigned To: ${assignedTo}</p>
+          </div>
+          <br>
+          <small class="tag is-primary">${status}</small>
+        </div>
+        <footer class="card-footer">
+          <a onclick="setStatusClosed('${id}')" class="is-warning card-footer-item">Close</a>
+          <a class="card-footer-item" onclick="deleteBug('${id}')">Delete</a>
+        </footer>
+      </div>
+        <br>`
+    }
+  }
+
   render () {
     return (
-      <div className="columns">
-        <div id="list-bugs" className="column is-medium"></div>
+      <div className="columns" onLoad={this.fetchBugs}>
+        <div id="listBugs" className="column is-medium">
+        </div>
       </div>
     )
   }
