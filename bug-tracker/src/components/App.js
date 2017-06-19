09feb6c2aa5638ec4d9fed242de60c473b732800
raynormw <tirtawiryaputra@yahoo.com>
2017-06-19 21:48:36 +0700

    list bugs with props done

--- bug-tracker/src/components/App.js
+++ bug-tracker/src/components/App.js
@@ -4,19 +4,39 @@ import Form from './Form';
 import List from './List';
 import Footer from './Footer';
 
+// Load Chance
+var Chance = require('chance');
+// Instantiate Chance so it can be used
+var chance = new Chance();
+
 class App extends React.Component {
   render() {
+    const bugs = this._getBugs();
     return (
       <div>
         <div className="container">
           <h1 className="title is-1">Bug Tracker <small>by HACKTIV8</small></h1>
           <Form />
           <hr />
-          <List />
+          {bugs}
           <Footer />
         </div>
       </div>
     );
+  }
+
+  _getBugs() {
+    let bugs = [];
+    if (localStorage.getItem('bugs') !== null) {
+      bugs = JSON.parse(localStorage.getItem('bugs'));
+    }
+
+    return bugs.map((bug) => {
+      return (
+        <List
+          description={bug.description} severity={bug.severity} status={bug.status} assignedTo={bug.assignedTo} id={bug.id} />
+      );
+    })
   }
 }
 
--- bug-tracker/src/components/List.js
+++ bug-tracker/src/components/List.js
@@ -4,7 +4,29 @@ class List extends React.Component {
   render () {
     return (
       <div className="columns">
-        <div className="column is-medium" id="listBugs">Test</div>
+        <div className="column is-medium">
+          <div className="card">
+            <header className="card-header">
+              <p className="card-header-title">
+                BugId: {this.props.id}
+              </p>
+            </header>
+            <div className="card-content">
+              <div className="content">
+                {this.props.description}&nbsp;
+                <span className="tag is-info">{this.props.severity}</span>
+                <p>Assigned To: {this.props.assignedTo}</p>
+              </div>
+              <br/>
+              <small className="tag is-primary">{this.props.status}</small>
+            </div>
+            <footer className="card-footer">
+              {/* <a onclick="setStatusClosed('${id}')" className="is-warning card-footer-item">Close</a>
+              <a className="card-footer-item" onclick="deleteBug('${id}')">Delete</a> */}
+            </footer>
+          </div>
+          <br/>
+        </div>
       </div>
     );
   }
