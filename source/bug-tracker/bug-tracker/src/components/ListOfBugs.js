73c2e8d53e13179151591aba7706bc84f23ddeba
ingelieur <hacktiv8@hacktiv8s-Mac-mini.local>
2017-06-19 16:29:49 +0700

    view is fine

---
+++ source/bug-tracker/bug-tracker/src/components/ListOfBugs.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class ListOfBugs extends React.Component {
+  render () {
+    return (
+      <div className="columns">
+        <div id="list-bugs" className="column is-medium"></div>
+      </div>
+    )
+  }
+}
+
+export default ListOfBugs
