839a8dba03a7817b3fadc9c9d00c4c618309c56d
ingelieur <ingelieur@gmail.com>
2017-06-20 08:56:03 +0700

    cleaning up

--- source/bug-tracker/bug-tracker/src/components/BugsForm.js
+++ source/bug-tracker/bug-tracker/src/components/BugsForm.js
@@ -61,42 +61,4 @@ function saveBug (e) {
   e.preventDefault()
 }
 
-// function fetchBugs () {
-//   let bugs = JSON.parse(localStorage.getItem('bugs')) || []
-  // let listBugsElement = document.getElementById('listBugs')
-  //
-  // listBugsElement.innerHTML = ''
-  //
-  // for(let i = 0; i < bugs.length; i++) {
-  //   let id = bugs[i].id
-  //   let desc = bugs[i].description
-  //   let severity = bugs[i].severity
-  //   let assignedTo = bugs[i].assignedTo
-  //   let status = bugs[i].status
-  //
-  //   listBugsElement.innerHTML += `<div class="card">
-  //     <header class="card-header">
-  //       <p class="card-header-title">
-  //       BugId: ${id}
-  //       </p>
-  //     </header>
-  //     <div class="card-content">
-  //       <div class="content">
-  //         ${desc}
-  //         <span class="tag is-info">${severity}</span>
-  //         <p>Assigned To: ${assignedTo}</p>
-  //       </div>
-  //       <br>
-  //       <small class="tag is-primary">${status}</small>
-  //     </div>
-  //     <footer class="card-footer">
-  //       <a onclick="setStatusClosed('${id}')" class="is-warning card-footer-item">Close</a>
-  //       <a class="card-footer-item" onclick="deleteBug('${id}')">Delete</a>
-  //     </footer>
-  //   </div>
-  //     <br>`
-  // }
-// }
-
-
 export default BugsForm
